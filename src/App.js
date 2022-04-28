604c2e44f81f6ee87b774a752f4c95df5211af3f
Joely Brito <joely_brito@outlook.com>
2022-04-28 15:20:13 -0300

    Adicionado card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './styles/reset.css';
-import './styles/App.css';
+import "./styles/reset.css";
+import "./styles/App.css";
 import Carousel from "./components/Carousel";
+import Card from "./components/Card";
 
 function App() {
-  
   return (
-    <Carousel/>
+    <>
+      <Carousel />
+      <Card />
+    </>
   );
 }
 
